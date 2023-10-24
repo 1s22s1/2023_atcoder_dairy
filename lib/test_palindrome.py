56a38151c067a90c157cd156850022bcf7fee8c6
1s22s1 <1s22s1@example.com>
2023-10-25 07:18:53 +0900

    Add test

--- lib/test_palindrome.py
+++ lib/test_palindrome.py
@@ -6,3 +6,15 @@ def test_5_palindrome_number():
     assert True == palindrome_5_number(11111)
     assert True == palindrome_5_number(11211)
     assert True == palindrome_5_number(11311)
+
+def base9(number):
+    result = ''
+
+    while number > 0:
+        result = str(number % 9) + result
+        number = number // 9
+
+    return int(result)
+
+def test_base9():
+    assert 2685 == base9(2021)
