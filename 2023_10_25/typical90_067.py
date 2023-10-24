3fefc7b249de090f864ce9bccff918b49c001070
1s22s1 <1s22s1@example.com>
2023-10-25 07:42:34 +0900

    RE1 Typical90 067

--- 2023_10_25/typical90_067.py
+++ 2023_10_25/typical90_067.py
@@ -1,5 +1,17 @@
 N, K = map(int, input().split())
 
+def base9(number):
+    result = ''
+
+    while number > 0:
+        result = str(number % 9) + result
+        number = number // 9
+
+    return int(result)
+
 for i in range(K):
     N = int(str(N), 8)
-    print(N)
+    N = base9(N)
+    N = int(str(N).replace('8', '5'))
+
+print(N)
