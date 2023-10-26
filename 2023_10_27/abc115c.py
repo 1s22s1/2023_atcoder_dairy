56797889b25b3ad7ceccfd1abff2d55249c9a80c
1s22s1 <1s22s1@example.com>
2023-10-27 08:17:21 +0900

    Do not solve ABC115 C

--- 2023_10_27/abc115c.py
+++ 2023_10_27/abc115c.py
@@ -3,3 +3,14 @@ Hn = []
 
 for _ in range(N):
     Hn.append(int(input()))
+
+Hn.sort()
+
+min = -1
+
+for i in range(N - K + 1):
+    diff = Hn[i + K - 1] - Hn[i]
+    if min > diff:
+        min = diff
+
+print(diff)
