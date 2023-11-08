c37368c4e8e8fbeb776865c29d98b114c00798ae
1s22s1 <1s22s1@example.com>
2023-11-09 08:16:24 +0900

    Add methods on string

--- stdlib/test_string.jl
+++ stdlib/test_string.jl
@@ -2,9 +2,27 @@ module TestString
     using Test
 
     function main()
-        @testset "plus" begin
-            @test 3 == 3
-            # @test Calculate.plus(2, 3) == 5
+        @testset "length" begin
+            @test length("呉起") == 2
+            # 兵庫ユカ『七月の心臓』
+            @test length("人形が川を流れていきました約束だからみたいな顔で") == 24
+        end
+
+        @testset "*" begin
+            @test "春秋" * "戦国" == "春秋戦国"
+            @test "尾道観" * "光" == "尾道観光"
+        end
+
+        @testset "uppercae" begin
+            @test uppercase("konoe") == "KONOE"
+            @test uppercase("Konoe") == "KONOE"
+            @test uppercase("KONOE") == "KONOE"
+        end
+
+        @testset "lowercase" begin
+            @test lowercase("konoe") == "konoe"
+            @test lowercase("Konoe") == "konoe"
+            @test lowercase("KONOE") == "konoe"
         end
     end
 end
