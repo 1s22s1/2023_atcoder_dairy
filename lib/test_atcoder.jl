31f31b8212143479556595eb9360db0e84bfea1b
1s22s1 <1s22s1@example.com>
2023-11-24 07:25:12 +0900

    Add nth

--- lib/test_atcoder.jl
+++ lib/test_atcoder.jl
@@ -2,7 +2,21 @@ module TestAtcoder
     using Test
 
     function main()
-        @test string(2) == "2"
+        @testset "From Int to String" begin
+            @test string(2) == "2"
+        end
+
+        @testset "From decimal to n-decimal" begin
+            @test string(11, base=2) == "1011"
+            @test string(11, base=3) == "102"
+            @test string(11, base=4) == "23"
+        end
+
+        @testset "From n-decimal to decimal" begin
+            @test parse(Int, "1011", base=2) == 11
+            @test parse(Int, "102", base=3) == 11
+            @test parse(Int, "23", base=4) == 11
+        end
     end
 end
 
